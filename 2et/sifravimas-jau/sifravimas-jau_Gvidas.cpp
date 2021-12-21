36b3efa2c8bb48f9c80dfe7d1e6c575be79a1ae7
GvidasVJG <62912063+GvidasVJG@users.noreply.github.com>
2021-12-21 03:56:57 +0200

    Pridėjau savo sprendimą sifravimui

---
+++ 2et/sifravimas-jau/sifravimas-jau_Gvidas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <cmath>
+
+int main(){
+    std::string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_,.";
+    std::map <char, char> dict;
+    for(int i = 0; i < abc.length(); ++i){
+        char from = abc[i], to;
+        int index = i+1;
+        index = int(std::pow(index, 3))%29;
+        dict.insert({abc[index], abc[i]});
+    }
+
+    std::string e;
+    std::cin >> e;
+    for(int l = 0; l < e.length(); ++l){
+        std::cout << dict[e[l]] << std::endl;
+    }
+}
